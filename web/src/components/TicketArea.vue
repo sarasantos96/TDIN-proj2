a24f624857d195067e081104c65f10ed2a15f96c
Unknown <joao1906vitor@gmail.com>
2018-05-19 11:42:57 +0100

    Added creation of a new Ticket

--- web/src/components/TicketArea.vue
+++ web/src/components/TicketArea.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="mainDiv">
     <el-row>
-      <el-col :span="5" :offset="1">
+      <el-col :span="10" :offset="1">
         <h3>Current Tickets:</h3>
+      </el-col>
+      <el-col :span="8" :offset="2">
+        <h3>Create new Ticket:</h3>
       </el-col>
     </el-row>
     <el-row>
@@ -18,12 +21,30 @@
             <div class="state">
               <div v-if="ticket.status === 'unassigned'">State: <el-tag type="info">Unassigned</el-tag></div>
               <div v-else-if="ticket.status === 'assigned to'">State: <el-tag>Assigned to</el-tag></div>
-              <div v-else-if="ticket.status === 'waiting for answers'">State: <el-tag type="warning">waiting for answers</el-tag></div>
+              <div v-else-if="ticket.status === 'waiting for answers'">Status: <el-tag type="warning">waiting for answers</el-tag></div>
               <div v-else>State: <el-tag type="success">Solved</el-tag></div>
 
             </div>
           </el-collapse-item>
         </el-collapse>
+      </el-col>
+      <el-col :span="8" :offset="2">
+        <el-row>
+          <el-form ref="newTicket" :model="newTicket">
+            <el-form-item label="Title">
+              <el-input placeholder="Title" v-model="newTicket.title"></el-input>
+            </el-form-item>
+            <el-form-item label="Description">
+              <el-input rows="5" placeholder="Enter description here" type="textarea" v-model="newTicket.description"></el-input>
+            </el-form-item>
+            <el-form-item class="ticket-button">
+              <el-button type="primary" @click="createTicket">Submit Ticket</el-button>
+            </el-form-item>
+          </el-form>
+        </el-row>
+        <el-row>
+
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -39,7 +60,50 @@ export default {
         name: '',
         email: ''
       },
-      tickets: []
+      tickets: [],
+      newTicket: {
+        title: '',
+        description: ''
+      }
+    }
+  },
+  methods: {
+    updateTable () {
+      let request = 'api/ticket/user/' + this.user.id
+
+      this.axios.get(request)
+        .then(response => {
+          this.tickets = response.data
+        })
+        .catch(error => {
+          console.log('GET request failed with error ' + error)
+        })
+    },
+    createTicket () {
+      if (this.newTicket.title === '' || this.newTicket.description === '') {
+        alert('You have to fill the title and description fields|')
+      }
+      else {
+        let newTicket = {
+          userId: this.user.id,
+          name: this.user.name,
+          email: this.user.email,
+          title: this.newTicket.title,
+          description: this.newTicket.description,
+          status: 'unassigned'
+        }
+
+        this.axios.post('api/ticket', newTicket)
+          .then(() => {
+            console.log('New ticket created successfully!')
+            this.updateTable()
+            this.newTicket.title = ''
+            this.newTicket.description = ''
+          })
+          .catch(error => {
+            console.log('POST request failed with error ' + error)
+          })
+      }
     }
   },
   mounted () {
@@ -52,8 +116,6 @@ export default {
     this.axios.get(request)
       .then(response => {
         this.tickets = response.data
-        console.log(this.tickets)
-        console.log(this.tickets.length)
       })
       .catch(error => {
         console.log('GET request failed with error ' + error)
@@ -77,4 +139,9 @@ export default {
     text-align: right;
     font-weight: bold;
   }
+
+  .ticket-button {
+    float: right;
+  }
+
 </style>
